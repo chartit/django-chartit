6712faa412d22e98e54042c56b219004ab7992de
Mr. Senko <atodorov@mrsenko.com>
2017-01-10 13:03:29 +0200

    flake8 fixes

--- demoproject/demoproject/chartdemo.py
+++ demoproject/demoproject/chartdemo.py
@@ -775,7 +775,8 @@ def combination_line_pie(_, title, code, doc, sidebar_items):
 
     def monthname(month_num):
         names = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
-                 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: u'Дек'}
+                 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov',
+                 12: u'Дек'}
         return names[month_num]
 
     cht = Chart(
